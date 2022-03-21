854ecff2159ec074730e700c03bcc0dbbf24d4d7
chengh <821635867@qq.com>
2022-03-21 22:19:55 +0800

    测试异常捕获

--- springMVC-demo1/src/main/java/com/chengh/mvc/controller/TestController.java
+++ springMVC-demo1/src/main/java/com/chengh/mvc/controller/TestController.java
@@ -1,17 +1,26 @@
 package com.chengh.mvc.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class TestController {
     @RequestMapping("/")
-    public String index(){
+    public String index() {
         return "index";
     }
 
     @RequestMapping("/test1")
-    public String test1(){
+    public String test1() {
         return "test1";
+    }
+
+    @GetMapping("/exception")
+    public String testException()  {
+        if (1 != 2) {
+            throw new RuntimeException();
+        }
+        return "success";
     }
 }
